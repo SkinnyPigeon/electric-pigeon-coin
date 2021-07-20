ff5674b11ed173ae92c5957d3dac8541f0304a48
skinnypigeon <philtheskinnypigeon@yahoo.co.uk>
2021-07-20 22:11:08 +0100

    added in some function comments

--- blockchain.py
+++ blockchain.py
@@ -2,14 +2,23 @@ blockchain = []
 
 
 def get_last_blockchain_value():
+    """ Returns the last value of the current blockchain. """
     return blockchain[-1]
 
 
 def add_value(transaction_amount, last_transaction=[1]):
+    """ Append a new value as well as the last clockchain to the blockchain.
+    
+    Arguments:
+        :transaction_amount: The amount that should be added. 
+        
+        :last_transaction: The last blockchain transaction (default [1]).
+    """
     blockchain.append([last_transaction, transaction_amount])
 
 
 def get_user_input():
+    """ Returns the input of the user (a new transaction amount) as a float. """
     return float(input('Your transaction amount please: '))
 
 
