0a565fb121b2ae4aafd0f4a378aa8d534d9a41d2
skinnypigeon <philtheskinnypigeon@yahoo.co.uk>
2021-07-29 22:25:21 +0100

    removed practice file

--- functions.py
+++
@@ -1,8 +0,0 @@
-def unlimited_arguments(*args, **keyword_args):
-    print(keyword_args)
-    for argument in args:
-        print(argument)
-
-
-unlimited_arguments(1, 2, 3, 4, 5, name='Euan', age=36)
-unlimited_arguments(*[1, 2, 3, 4, 5])
