e0b0405210a0d7e10e2f14ba0b594fc19ede2610
skinnypigeon <philtheskinnypigeon@yahoo.co.uk>
2021-08-04 13:55:01 +0100

    cleaned up the output function

--- blockchain.py
+++ blockchain.py
@@ -182,8 +182,7 @@ while waiting_for_input:
     print("1: Add a new transaction value")
     print("2: Mine a new block")
     print("3: Output the blockchain blocks")
-    print("4: Output the participants")
-    print("5: Check transaction validity")
+    print("4: Check transaction validity")
     print("q: Quit!")
 
     user_choice = get_user_choice()
@@ -205,9 +204,6 @@ while waiting_for_input:
         print_blockchain_elements()
 
     elif user_choice == '4':
-        print(participants)
-
-    elif user_choice == '5':
         if verify_transactions():
             print('All transactions are valid')
         else:
