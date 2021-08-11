1ee0db54d7efc60b3edaa5a17802e5ae0afd916a
skinnypigeon <philtheskinnypigeon@yahoo.co.uk>
2021-08-11 23:23:08 +0100

    changing over to the right db address

--- code/database/access.py
+++ code/database/access.py
@@ -6,15 +6,16 @@ load_dotenv()
 # PGPASSWORD=os.getenv("PGPASSWORD")
 # DATABASE_URL = os.getenv('DATABASE_URL')
 
-PGUSER=os.environ["PGUSER"]
-PGPASSWORD=os.environ["PGPASSWORD"]
+# PGUSER=os.environ["PGUSER"]
+# PGPASSWORD=os.environ["PGPASSWORD"]
 DATABASE=os.environ["DATABASE"]
 
 from sqlalchemy import MetaData, create_engine, insert, select
 from sqlalchemy.ext.automap import automap_base
 
 def save_user_to_db(public_key, private_key, port):
-    engine = create_engine(f'{DATABASE}')
+    # engine = create_engine(f'{DATABASE}')
+    engine = create_engine(f'postgresql+psycopg2://{PGUSER}:{PGPASSWORD}@localhost:5432/blockchain')
     metadata = MetaData(bind=engine)
     metadata.reflect(engine)
     Base = automap_base(metadata=metadata)
@@ -35,7 +36,8 @@ def save_user_to_db(public_key, private_key, port):
         return False
 
 def load_user_from_db(public_key):
-    engine = create_engine(f'{DATABASE}')
+    # engine = create_engine(f'{DATABASE}')
+    engine = create_engine(f'postgresql+psycopg2://{PGUSER}:{PGPASSWORD}@localhost:5432/blockchain')
     metadata = MetaData(bind=engine)
     metadata.reflect(engine)
     Base = automap_base(metadata=metadata)
@@ -53,7 +55,8 @@ def load_user_from_db(public_key):
 
 
 def load_node_from_db(port):
-    engine = create_engine(f'{DATABASE}')
+    engine = create_engine(f'postgresql+psycopg2://{PGUSER}:{PGPASSWORD}@localhost:5432/blockchain')
+    # engine = create_engine(f'{DATABASE}')
     metadata = MetaData(bind=engine)
     metadata.reflect(engine)
     Base = automap_base(metadata=metadata)
