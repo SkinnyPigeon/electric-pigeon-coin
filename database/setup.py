faf10cf02cfe867fec0dca88b644f7a7ad18aaec
skinnypigeon <philtheskinnypigeon@yahoo.co.uk>
2021-08-11 18:02:15 +0100

    table is not initialising

--- database/setup.py
+++ database/setup.py
@@ -10,19 +10,19 @@ DATABASE=os.environ["DATABASE"]
 
 from sqlalchemy import Column, String, BigInteger, Integer, create_engine
 from sqlalchemy.orm import declarative_base
-Base = declarative_base()
 
-engine = create_engine(f'postgresql+psycopg2://{PGUSER}:{PGPASSWORD}@{DATABASE}')
-
-class BCWallet(Base):
-    __tablename__ = 'wallets'
-    id = Column(BigInteger, primary_key=True, autoincrement=True)
-    public_key=Column(String, primary_key=True)
-    private_key=Column(String)
-    port = Column(Integer)
 
 
 def initialise_db():
+    Base = declarative_base()
+    engine = create_engine(f'postgresql+psycopg2://{PGUSER}:{PGPASSWORD}@{DATABASE}')
+    print(f"ENGINE: {engine}")
+    class BCWallet(Base):
+        __tablename__ = 'wallets'
+        id = Column(BigInteger, primary_key=True, autoincrement=True)
+        public_key=Column(String, primary_key=True)
+        private_key=Column(String)
+        port = Column(Integer)
     try:
         Base.metadata.create_all(engine)
         engine.dispose()
