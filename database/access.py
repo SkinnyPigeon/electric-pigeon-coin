7ff28e0b4645a30a3f3044c66b345e9c5c6fe2d5
skinnypigeon <philtheskinnypigeon@yahoo.co.uk>
2021-08-11 18:13:41 +0100

    that might be db working

--- database/access.py
+++ database/access.py
@@ -14,7 +14,7 @@ from sqlalchemy import MetaData, create_engine, insert, select
 from sqlalchemy.ext.automap import automap_base
 
 def save_user_to_db(public_key, private_key, port):
-    engine = create_engine(f'postgresql+psycopg2://{PGUSER}:{PGPASSWORD}@{DATABASE}')
+    engine = create_engine(f'{DATABASE}')
     metadata = MetaData(bind=engine)
     metadata.reflect(engine)
     Base = automap_base(metadata=metadata)
@@ -35,7 +35,7 @@ def save_user_to_db(public_key, private_key, port):
         return False
 
 def load_user_from_db(public_key):
-    engine = create_engine(f'postgresql+psycopg2://{PGUSER}:{PGPASSWORD}@{DATABASE_URL}')
+    engine = create_engine(f'{DATABASE}')
     metadata = MetaData(bind=engine)
     metadata.reflect(engine)
     Base = automap_base(metadata=metadata)
@@ -53,7 +53,7 @@ def load_user_from_db(public_key):
 
 
 def load_node_from_db(port):
-    engine = create_engine(f'postgresql+psycopg2://{PGUSER}:{PGPASSWORD}@{DATABASE_URL}')
+    engine = create_engine(f'{DATABASE}')
     metadata = MetaData(bind=engine)
     metadata.reflect(engine)
     Base = automap_base(metadata=metadata)
--- database/setup.py
+++ database/setup.py
@@ -4,29 +4,31 @@ load_dotenv()
 # PGUSER=os.getenv("PGUSER")
 # PGPASSWORD=os.getenv("PGPASSWORD")
 # DATABASE_URL = os.getenv('DATABASE_URL')
-PGUSER=os.environ["PGUSER"]
-PGPASSWORD=os.environ["PGPASSWORD"]
-DATABASE=os.environ["DATABASE"]
+DATABASE=os.getenv('DATABASE')
+
+# PGUSER=os.environ["PGUSER"]
+# PGPASSWORD=os.environ["PGPASSWORD"]
+# DATABASE=os.environ["DATABASE"]
+# DATABASE_URL=os.environ["DATABASE_URL"]
 
 from sqlalchemy import Column, String, BigInteger, Integer, create_engine
 from sqlalchemy.orm import declarative_base
 
+Base = declarative_base()
+# engine = create_engine(f'postgresql+psycopg2://{PGUSER}:{PGPASSWORD}@{DATABASE}')
+engine = create_engine(f'{DATABASE}')
+print(f"ENGINE: {engine}")
 
+class BCWallet(Base):
+    __tablename__ = 'wallets'
+    id = Column(BigInteger, primary_key=True, autoincrement=True)
+    public_key=Column(String, primary_key=True)
+    private_key=Column(String)
+    port = Column(Integer)
+
+
+Base.metadata.create_all(engine)
+engine.dispose()
 
 def initialise_db():
-    Base = declarative_base()
-    engine = create_engine(f'postgresql+psycopg2://{PGUSER}:{PGPASSWORD}@{DATABASE}')
-    print(f"ENGINE: {engine}")
-    class BCWallet(Base):
-        __tablename__ = 'wallets'
-        id = Column(BigInteger, primary_key=True, autoincrement=True)
-        public_key=Column(String, primary_key=True)
-        private_key=Column(String)
-        port = Column(Integer)
-    # try:
-    Base.metadata.create_all(engine)
-    engine.dispose()
     return {'message': 'Database initialised'}, 200
-    # except:
-    #     engine.dispose()
-    #     return {'message': 'Database initialisation failed'}, 500
